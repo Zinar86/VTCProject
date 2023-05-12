a58f5180856385322f1769336a04403ab19cd701
Nicolas14200 <heutte.nicolas@laposte.net>
2023-05-12 10:37:22 +0200

    password gateway

--- src/adapters/gateways/Bcrypt/ComparePasswordCrypt.ts
+++
@@ -1,6 +0,0 @@
-import { compareSync } from 'bcrypt';
-export class ComparePasswordCryptPassword {
-    execute(password: string, passwordCrypt: string){
-        return compareSync(password, passwordCrypt);
-    }
-}
--- src/adapters/gateways/Bcrypt/CryptPassword.ts
+++
@@ -1,8 +0,0 @@
-import { genSaltSync, hashSync } from 'bcrypt';
-export class CryptPassword {
-    saltRounds = genSaltSync(10);
-    execute(password: string){
-        return hashSync(password, this.saltRounds)
-    }
-
-}
