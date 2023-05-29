5409769fe4f306b6d68d563a4a2f095854ee8766
Drixx05 <drixion05@gmail.com>
2023-05-29 18:43:20 +0200

    mapperdriver

--- src/app/router/driver/DriverRouter.ts
+++ src/app/router/driver/DriverRouter.ts
@@ -2,11 +2,13 @@ import {Request, Response, Router} from "express";
 import {DriverRepository} from "../../../core/domain/repositories/DriverRepository";
 import {InMemoryDriverRepository} from "../../../core/__test__/repository/InMemoryDriverRepository";
 import {BecomeADriver} from "../../../core/usecase/driver/BecomeADriver";
+import { DriverApiResponseMapper } from "../user/mappers/DriverApiResponseMapper";
 
 export const driverRouter : Router = Router();
 
 const driverRepository : DriverRepository = new InMemoryDriverRepository()
 const becomeadriver = new BecomeADriver(driverRepository)
+const driverApiResponseMapper = new DriverApiResponseMapper();
 driverRouter.post("/become/" ,async (req: Request, res: Response)=>{
  try {
     const driver = await becomeadriver.execute({
@@ -18,7 +20,8 @@ driverRouter.post("/become/" ,async (req: Request, res: Response)=>{
         kbis: req.body.kbis,
         carRegistrationDocument: req.body.carRegistrationDocument,
     })
-    return res.status(200).send(driver);
+    const toApiResponse = driverApiResponseMapper.fromDomain(driver);
+    return res.status(200).send(toApiResponse);
  }
  catch (error){
      return res.status(401).send(error.message)}
