7c69f73041eeda22976f5127bd3c0b5c1cce5345
Drixx05 <drixion05@gmail.com>
2023-05-30 15:31:10 +0200

    Car Model added

---
+++ src/adapters/repositories/mongodb/models/CarModel.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose'
+
+const carSchema = new mongoose.Schema({
+    id : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    registration :{
+    type: String,
+    required: true,
+    unique: true,
+    },
+    model : {
+        type: String,
+        required: true,
+    },
+    picture : {
+        type: String
+    }
+})
