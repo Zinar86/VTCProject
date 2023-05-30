a68821ff037268694c70c2fbf997ed861c423122
Drixx05 <drixion05@gmail.com>
2023-05-30 15:01:49 +0200

    Added Mongoose Ride model

--- src/adapters/repositories/mongodb/models/RideModel.ts
+++ src/adapters/repositories/mongodb/models/RideModel.ts
@@ -6,4 +6,38 @@ const rideSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    userId : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    startAddress : {
+        long: Number,
+        lat: Number,
+        streetAddress: String,
+        city: String,
+        zipCode: String,
+    },
+    endAddress : {
+        long: Number,
+        lat: Number,
+        streetAddress: String,
+        city: String,
+        zipCode: String,
+    },
+    priceEstimation : {
+        type : Number,
+        required : true,
+    },
+    paymentMethod : {
+        type : Number,
+        enum : ['cash'],
+        default : 'cash',
+    },
+    rideType : {
+        type : String,
+        enum : ['eco', 'comfort', 'berline', 'van', 'green']
+    },
 })
+
+export const RideModel = mongoose.model('ride', rideSchema);
